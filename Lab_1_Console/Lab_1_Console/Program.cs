77aa1d0d7eb3ae12ad0eb2ea63dd55a645f932af
Annin Pavel <anninpavel2014@gmail.com>
2016-01-04 14:04:37 +0300

    Добавлена лабораторная работа №1 (Консольная).

---
+++ Lab_1_Console/Lab_1_Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_1_Console {
+    class Program {
+        static void Main(string[] args) {
+            ShowMessage(ResString.Message);
+
+            ShowMessage(ResString.PressAnyKey);
+            Console.ReadKey();
+        }
+
+        public static void ShowMessage(string msg) {
+            if (msg != null) {
+                Console.WriteLine(msg);
+            }
+        }
+        
+    }
+}
